f1c588542d7b8a1473c9bc1819e46f1f5b8e5783
NauelG <g.nauel@gmail.com>
2018-08-23 19:44:26 +0200

    Usuario i token listo

---
+++ routes/login.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+var app = express();
+
+var Usuario = require('../models/usuario');
+
+app.post('/', (req, res, next) => {
+
+    var body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error buscando usuarios',
+                errors: err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Autenticacion incorrecta (dev - EMAIL)',
+                errors: err
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Autenticacion incorrecta (dev - PASSWORD)',
+                errors: err
+            });
+        }
+
+        // TODO Crear token
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+            token: token,
+            id: usuarioDB._id
+        });
+    });
+
+});
+
+module.exports = app;
---
+++ routes/usuario.js
@@ -0,0 +1,176 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+var app = express();
+
+var Usuario = require('../models/usuario');
+
+// ==================================
+// Obtener todos los usuarios
+// ==================================
+app.get('/', (req, res, next) => {
+
+    Usuario.find({}, 'nombre email img role')
+        .exec(
+            (err, usuarios) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error cargando usuarios',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    usuarios: usuarios
+                });
+            });
+
+});
+
+// ==================================
+// Verificar token
+// ==================================
+// !==================================
+// ! Es un ejemplo
+// !==================================
+// app.use('/', (req, res, next) => {
+
+//     var token = req.query.token;
+
+//     jwt.verify(token, SEED, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).json({
+//                 ok: false,
+//                 mensaje: 'Error de login',
+//                 errors: err
+//             });
+//         }
+//         next();
+//     });
+
+
+// });
+
+// ==================================
+// Actualizar usuario
+// ==================================
+app.put('/:id', mdAutenticacion.verificaToken, (req, res, next) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error buscando usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: `No existe ningun usuario con el id: ${id}`,
+                errors: { message: 'No existe ningun usuario con el id especificado' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error actualizando usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+
+        });
+    })
+
+
+});
+
+// ==================================
+// Crear un nuevo usuario
+// ==================================
+app.post('/', mdAutenticacion.verificaToken, (req, res, next) => {
+
+    var body = req.body;
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioGuardado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error guardado usuario',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            usuario: usuarioGuardado,
+            usuarioTKN: req.usuario
+        });
+
+    });
+
+});
+
+// ==================================
+// Borrar usuario por su ID
+// ==================================
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res, next) => {
+
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error borrando usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: `No existe ningún usuario con el ID: ${id}`,
+                errors: { message: 'No existe ningún usuario con ese ID' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+});
+
+
+module.exports = app;
